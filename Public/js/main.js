25891323208f45fddcf9a7545974ae077e7e890a
Tyler Black <blacktylera@gmail.com>
2015-02-05 12:37:31 -0600

    Added more tests. Appended table to document. Wrapped elements in jQuery

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,24 +1,23 @@
-function addStockToTable(stock) {
+function addStockToTable(stock){
   var $row = $('<tr></tr>');
+
+  $row.append('<td>' + stock.Name + '</td>');
+  $row.append('<td>' + stock.Symbol + '</td>');
+  $row.append('<td>' + stock.LastPrice + '</td>');
+
   $('tbody').append($row);
+
+  return $row;
 }
 
-
-
-
-
-
-function getStock (symbol, cb) {
+function getStock(symbol, cb) {
   var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol;
-  $.get(url, function (res){
-  return cb(res);
-  },'jsonp');
-};
 
-
+  $.get(url, function(res){
+    return cb(res);
+  }, 'jsonp');
+}
 
 function hello() {
   return 'world';
 }
-
-
--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -1,43 +1,48 @@
-/* jshint mocha: true, expr: true, strict: false */
-
+/* jshint mocha: true, expr: true, strict: false, undef: false */
 
 describe('test suite', function () {
   it('should assert true', function () {
     true.should.be.true;
     false.should.be.false;
   });
- });
+});
 
 describe('hello', function () {
   it('should return world', function () {
     hello().should.equal('world');
-  })
-})
+  });
+});
 
-
-describe('GetStock', function () {
+describe('getStock', function () {
   it('should return a stock object', function (done) {
-    getStock('AAPL', function(stock){
+    getStock('AAPL', function (stock) {
       stock.Name.should.equal('Apple Inc');
       done();
     });
   });
-
-
   it('should return another stock object', function (done) {
     getStock('MSFT', function (stock) {
       stock.Name.should.equal('Microsoft Corp');
       done();
-
     });
   });
 });
 
 describe('addStockToTable', function () {
-  it('should use stock info to append to table', function () {
-    var stock = {Name: "Supercorp", Symbol: "SCRP", LastPrice: 13.34};
+  it('should add a row table', function () {
+    var stock = { Name: 'SuperCorp', Symbol: 'SCRP', LastPrice: 12.34 };
     $('tr').length.should.equal(0);
     addStockToTable(stock);
     $('tr').length.should.equal(1);
   });
-})
+  it('should use stock data in the appended row', function () {
+    var stock = { Name: 'SuperCorp', Symbol: 'SCRP', LastPrice: 12.34 },
+        $row  = addStockToTable(stock),
+        $tds  = $row.find('td');
+
+    $tds.length.should.equal(3);
+    $($tds[0]).text().should.equal('SuperCorp');
+    $($tds[1]).text().should.equal('SCRP');
+    $($tds[2]).text().should.equal('12.34');
+  });
+});
