6a5bb47cd93e9179696b6a9c3dc8af19ecef616c
Tyler Black <blacktylera@gmail.com>
2015-02-05 10:09:09 -0600

    Added test suite

---
+++ test/spec/main.test.js
@@ -0,0 +1,11 @@
+/* jshint mocha: true, expr: true, strict: false */
+
+
+describe('test suite', function () {
+  it('should assert true', function () {
+    true.should.be.true;
+    false.should.be.false;
+  });
+ });
+
+
