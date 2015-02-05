a616d3777b90b350cbd9176d199aebb3b21a712e
Tyler Black <blacktylera@gmail.com>
2015-02-05 12:07:53 -0600

    Added initial jQuery and JavaScript while implementing new tests

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -13,3 +13,31 @@ describe('hello', function () {
     hello().should.equal('world');
   })
 })
+
+
+describe('GetStock', function () {
+  it('should return a stock object', function (done) {
+    getStock('AAPL', function(stock){
+      stock.Name.should.equal('Apple Inc');
+      done();
+    });
+  });
+
+
+  it('should return another stock object', function (done) {
+    getStock('MSFT', function (stock) {
+      stock.Name.should.equal('Microsoft Corp');
+      done();
+
+    });
+  });
+});
+
+describe('addStockToTable', function () {
+  it('should use stock info to append to table', function () {
+    var stock = {Name: "Supercorp", Symbol: "SCRP", LastPrice: 13.34};
+    $('tr').length.should.equal(0);
+    addStockToTable(stock);
+    $('tr').length.should.equal(1);
+  });
+})
